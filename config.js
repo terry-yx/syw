037089fd3f4ff0ec659499fbb43659645bd8fba7
terry-yx <root745200@163.com>
2025-09-14 00:04:50 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+window.scencename = '海天遊';
+window.geturl = [
+'http://xxxxxxxx:6780/cgi_bin',
+]
+window.appId = 'wx1';
+window.newPlatform = false;
+window.CardToPoint=1;
+window.PointToCard=1;
+window.openChat = false;//打开聊天室
+window._canSavePlayType = true; //保存玩法
+window._openTgzq = 0;
+window._isKaixin = true; //
+window._showHallSlideLeft=1;
+window._shoeMemberMoreInfo=0;
+window._onlyCreatorCreate=0;
---
+++ myconfig.js
@@ -0,0 +1,17 @@
+window.scencename = '逍遥游';
+window.geturl = [
+//'http://106.15.249.196:6780/cgi_bin',
+'http://192.140.171.213:6780/cgi_bin',//gf
+//'https://cgi.a6bd.cn:6770/cgi_bin',
+]
+window.appId = 'wx1';
+window.newPlatform = false;
+window.CardToPoint=1;
+window.PointToCard=1;
+window.openChat = false;//打开聊天室
+window._canSavePlayType = true; //保存玩法
+window._openTgzq = 0;
+window._isKaixin = true; //
+window._showHallSlideLeft=1;
+window._shoeMemberMoreInfo=0;
+window._onlyCreatorCreate=0;
